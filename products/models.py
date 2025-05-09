27956fa7a194f51f60eca25cf56b1857d5ca927e
Abdkhateeb <abdkhateeb@gmail.com>
2025-05-09 11:46:56 +0200

    updated description field to be longer 5000

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     short_description = models.TextField(max_length=300)
-    product_description = models.TextField(max_length=1000)
+    product_description = models.TextField(max_length=5000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.SmallIntegerField(default=0)
 
