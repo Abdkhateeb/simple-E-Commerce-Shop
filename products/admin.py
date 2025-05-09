3e82cb178ceec21f8acca93b99a168edc97f623b
Abdkhateeb <abdkhateeb@gmail.com>
2025-05-09 09:39:37 +0200

    product_model added

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    search_fields = ('name',)
+    list_filter = ('stock',)
+    ordering = ('-price',)
+    list_per_page = 10  # Number of products per page in the admin list view
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    short_description = models.TextField(max_length=300)
+    product_description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
